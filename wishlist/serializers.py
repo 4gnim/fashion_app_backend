5e241cf3bc96b3948caf8c5bea2f2d71865b9e28
4gnim <agnimusadad06@gmail.com>
2025-01-14 21:17:09 +0700

    first commit

---
+++ wishlist/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from . import models
+
+class WishListSerializers(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField(source='product.id')
+    title = serializers.ReadOnlyField(source='product.title')
+    description = serializers.ReadOnlyField(source='product.description')
+    is_featured = serializers.ReadOnlyField(source='product.is_featured')
+    clothesType = serializers.ReadOnlyField(source='product.clothesType')
+    price = serializers.ReadOnlyField(source='product.price')
+    ratings = serializers.ReadOnlyField(source='product.ratings')
+    category = serializers.ReadOnlyField(source='product.category.id')
+    brand = serializers.ReadOnlyField(source='product.brand.id')
+    colors = serializers.ReadOnlyField(source='product.colors')
+    sizes = serializers.ReadOnlyField(source='product.sizes')
+    imageUrls = serializers.ReadOnlyField(source='product.imageUrls')
+    created_at = serializers.ReadOnlyField(source='product.created_at')
+
+    class Meta:
+        model = models.Wishlist
+        fields = ['id', 'title', 'price', 'description', 'is_featured', 'clothesType', 'ratings', 'category', 'colors', 'sizes', 'imageUrls', 'created_at', 'brand']
